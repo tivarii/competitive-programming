e0479193acefdaeec813d6441dab3fde371c82cc
tivarii <adarshtiwati3576@gmail.com>
2025-08-02 01:40:47 +0530

    added binarylift and generic segment tree

---
+++ genericSegmentTree.cpp
@@ -0,0 +1,232 @@
+// Credits to HealthyUG for the inspiration.
+// Segment Tree with Point Updates and Range Queries
+// Supports multiple Segment Trees with just a change in the Node and Update
+// Very few changes required everytime
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+
+#ifndef ONLINE_JUDGE
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V>
+void _print(pair<T, V> p);
+template <class T>
+void _print(vector<T> v);
+template <class T>
+void _print(set<T> v);
+template <class T, class V>
+void _print(map<T, V> v);
+template <class T>
+void _print(multiset<T> v);
+
+template <class T, class V>
+void _print(pair<T, V> p)
+{
+    cerr << "{";
+    _print(p.ff);
+    cerr << ",";
+    _print(p.ss);
+    cerr << "}";
+}
+template <class T>
+void _print(vector<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(set<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(multiset<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T, class V>
+void _print(map<T, V> v)
+{
+    cerr << "[ ";
+    for (auto i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+template <typename Node, typename Update>
+struct SegTree
+{
+    vector<Node> tree;
+    vector<ll> arr; // type may change
+    int n;
+    int s;
+    SegTree(int a_len, vector<ll> &a)
+    { // change if type updated
+        arr = a;
+        n = a_len;
+        s = 1;
+        while (s < 2 * n)
+        {
+            s = s << 1;
+        }
+        tree.resize(s);
+        fill(all(tree), Node());
+        build(0, n - 1, 1);
+    }
+    void build(int start, int end, int index) // Never change this
+    {
+        if (start == end)
+        {
+            tree[index] = Node(arr[start]);
+            return;
+        }
+        int mid = (start + end) / 2;
+        build(start, mid, 2 * index);
+        build(mid + 1, end, 2 * index + 1);
+        tree[index].merge(tree[2 * index], tree[2 * index + 1]);
+    }
+    void update(int start, int end, int index, int query_index, Update &u) // Never Change this
+    {
+        if (start == end)
+        {
+            u.apply(tree[index]);
+            return;
+        }
+        int mid = (start + end) / 2;
+        if (mid >= query_index)
+            update(start, mid, 2 * index, query_index, u);
+        else
+            update(mid + 1, end, 2 * index + 1, query_index, u);
+        tree[index].merge(tree[2 * index], tree[2 * index + 1]);
+    }
+    Node query(int start, int end, int index, int left, int right)
+    { // Never change this
+        if (start > right || end < left)
+            return Node();
+        if (start >= left && end <= right)
+            return tree[index];
+        int mid = (start + end) / 2;
+        Node l, r, ans;
+        l = query(start, mid, 2 * index, left, right);
+        r = query(mid + 1, end, 2 * index + 1, left, right);
+        ans.merge(l, r);
+        return ans;
+    }
+    void make_update(int index, ll val)
+    {                                    // pass in as many parameters as required
+        Update new_update = Update(val); // may change
+        update(0, n - 1, 1, index, new_update);
+    }
+    Node make_query(int left, int right)
+    {
+        return query(0, n - 1, 1, left, right);
+    }
+};
+
+struct Node1
+{
+    ll val; // may change
+    Node1()
+    {            // Identity element
+        val = 0; // may change
+    }
+    Node1(ll p1)
+    {             // Actual Node
+        val = p1; // may change
+    }
+    void merge(Node1 &l, Node1 &r)
+    {                        // Merge two child nodes
+        val = l.val + r.val; // may change
+    }
+};
+
+struct Update1
+{
+    ll val; // may change
+    Update1(ll p1)
+    {             // Actual Update
+        val = p1; // may change
+    }
+    void apply(Node1 &a)
+    {                // apply update to given node
+        a.val = val; // may change
+    }
+};
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+#endif
+    fastio();
+    // Your code here
+    vector<ll> arr = {1, 2, 3, 4, 5}; // Example array
+
+    SegTree<Node1, Update1> seg1(arr.size(), arr);
+
+    cout << seg1.make_query(0, 3).val << nline;
+    
+    // Query sum from index 0 to 3
+
+    seg1.make_update(2,12);
+    cout<<seg1.make_query(2,2).val<<nline; // Query single index 2 after update
+                                                // Create segment tree with the array
+    return 0;
+}
